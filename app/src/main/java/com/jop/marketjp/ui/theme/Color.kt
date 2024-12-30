f44c24908b4c6b1a167c0261443ca058b0daa4ae
J. Mauricio <joffree88@gmail.com>
2024-12-29 19:01:13 -0500

    colors app theme

--- app/src/main/java/com/jop/marketjp/ui/theme/Color.kt
+++ app/src/main/java/com/jop/marketjp/ui/theme/Color.kt
@@ -9,3 +9,15 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val OrangeLight = Color(0xFFFFA726) // Naranja claro (Primario en tema claro)
+val OrangeDark = Color(0xFFEF6C00) // Naranja oscuro (Primario en tema oscuro)
+val OrangeAccent = Color(0xFFFFD54F) // Naranja pastel para acentos
+
+val NeutralLight = Color(0xFFF5F5F5) // Fondo claro
+val NeutralDark = Color(0xFF212121) // Fondo oscuro
+val TextLight = Color(0xFF212121) // Texto oscuro para tema claro
+val TextDark = Color(0xFFF5F5F5) // Texto claro para tema oscuro
+val Secondary = Color(0xFFFFCC80) // Naranja pálido como secundario
+val ErrorColor = Color(0xFFD32F2F) // Rojo para errores
--- app/src/main/java/com/jop/marketjp/ui/theme/Theme.kt
+++ app/src/main/java/com/jop/marketjp/ui/theme/Theme.kt
@@ -16,26 +16,31 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = OrangeDark,
+    secondary = Secondary,
+    tertiary = OrangeAccent,
+    background = NeutralDark,
+    surface = NeutralDark,
+    onPrimary = TextDark,
+    onSecondary = TextDark,
+    onTertiary = TextDark,
+    onBackground = TextDark,
+    onSurface = TextDark
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = OrangeLight,
+    secondary = Secondary,
+    tertiary = OrangeAccent,
+    background = NeutralLight,
+    surface = NeutralLight,
+    onPrimary = TextLight,
+    onSecondary = TextLight,
+    onTertiary = TextLight,
+    onBackground = TextLight,
+    onSurface = TextLight
 )
+
 
 @Composable
 fun MarketJPTheme(
